1a4f3a8295ee99269bcae0672223ab1e43d33680
vyeung <vl.yeung@yahoo.com>
2018-04-21 23:34:16 -0400

    added speaker routes

--- app/app.js
+++ app/app.js
@@ -6,7 +6,17 @@ var dataFile = require("./data/data.json");
 //default to port 3000 if not specifying one
 app.set("port", process.env.PORT || 3000);
 
+//homepage
 app.get("/", function(req, res)
+{
+    res.send(`
+        <h1>Welcome</h1>
+        <p>Roux Academy Meetups gets artists together from all walks of life</p>
+    `);
+});
+
+//speakers route
+app.get("/speakers", function(req, res)
 {
     var info = "";
     
@@ -27,23 +37,24 @@ app.get("/", function(req, res)
     res.send(`<h1>Roux Academy Meetups</h1>${info}`);
 });
 
+//individual speaker route. path has a speakerid var that we made.
+//since we have 3 ppl, speakerid can be 0,1,2 
+app.get("/speakers/:speakerid", function(req, res)
+{
+    //how speaker gets its info
+    var speaker = dataFile.speakers[req.params.speakerid];
+
+    //respond with info about an individual speaker
+    res.send(`
+        <h1>${speaker.title}</h1>
+        <h2>with ${speaker.name}</h2>
+        <p>${speaker.summary}</p>
+    `);
+});
+
 var server = app.listen(app.get("port"), function()
 {
     console.log("listening on port " + app.get("port"));
 });
 
 
-
-/* non express way
-var http = require("http");
-
-var myServer = http.createServer(function(req, res)
-{                                        //mime type
-    res.writeHead(200, {"Content-Type": "text/plain"});
-    res.write("Roux Meetups");
-    res.end();
-});
-
-myServer.listen(3000);
-console.log("listening on 3000");
-*/
