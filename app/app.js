17e3aac2c227a32c501400278c018d20e792fbe7
vyeung <vl.yeung@yahoo.com>
2018-04-21 23:10:54 -0400

    print speaker info

--- app/app.js
+++ app/app.js
@@ -1,13 +1,36 @@
 var express = require("express");
 var app = express();
+var dataFile = require("./data/data.json");
+
+//allow for a flexible port number by setting an environment variable. 
+//default to port 3000 if not specifying one
+app.set("port", process.env.PORT || 3000);
 
 app.get("/", function(req, res)
 {
-    res.send("<h1>Roux Academy Meetups<\h1>");
+    var info = "";
+    
+    dataFile.speakers.forEach(function(item)
+    {
+        //template string
+        info += `
+        <ul>
+            <li>
+                <h2>${item.name}</h2> 
+                <p>${item.summary}</p>
+            </li>
+        </ul>
+        `;
+    });
+
+    //print info after h1 header
+    res.send(`<h1>Roux Academy Meetups</h1>${info}`);
 });
 
-app.listen(3000);
-console.log("listening on 3000");
+var server = app.listen(app.get("port"), function()
+{
+    console.log("listening on port " + app.get("port"));
+});
 
 
 
