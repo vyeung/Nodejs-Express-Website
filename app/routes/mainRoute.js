66558ade5f5d01635c00e2db88d51c2c1bcd8820
vyeung <vl.yeung@yahoo.com>
2018-04-22 16:55:50 -0400

    added artwork on homepage

--- app/routes/mainRoute.js
+++ app/routes/mainRoute.js
@@ -4,11 +4,21 @@ var router = express.Router();  //create instance of Router object
 //homepage
 router.get("/", function(req, res)
 {
+    var data = req.app.get("appData");
+    var homepagePhotos = [];
+
+    //show all artwork on homepage
+    data.speakers.forEach(function(item)
+    {
+        homepagePhotos = homepagePhotos.concat(item.artwork);
+    })
+
     //render our homepage with index.ejs specified in app.js
     res.render("index",
     {
         //some local variables that are used in index.ejs
         pageTitle: "Home",
+        artwork: homepagePhotos,
         pageID: "home"
     });
 });
