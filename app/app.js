1b9af554369daa715416b15dd0fb0af3f671eb58
vyeung <vl.yeung@yahoo.com>
2018-04-23 19:14:24 -0400

    finished chat room

--- app/app.js
+++ app/app.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var app = express();
 var dataFile = require("./data/data.json");
+var io = require("socket.io")();
 
 //allow for a flexible port number by setting an environment variable. 
 //default to port 3000 if not specifying one
@@ -32,4 +33,13 @@ var server = app.listen(app.get("port"), function()
     console.log("listening on port " + app.get("port"));
 });
 
+io.attach(server);  //socket.io and server are now connected
 
+io.on("connection", function(socket)
+{
+    console.log("User Connected");
+    socket.on("postMessage", function(data)
+    {
+        io.emit("updateMessages", data);
+    });
+});
--- app/public/js/chat.js
+++ app/public/js/chat.js
@@ -2,33 +2,42 @@ var socket = io();
 var chatUsername = document.querySelector('#chat-username');
 var chatMessage = document.querySelector('#chat-message');
 
-socket.on('connect', function() {
+socket.on('connect', function() 
+{
   var chatForm = document.forms.chatForm;
 
   if (chatForm) {
     chatForm.addEventListener('submit', function(e) {
+      //don't reload page after submitting message
       e.preventDefault();
-      socket.emit('postMessage',{
+      
+      socket.emit('postMessage', {
         username: chatUsername.value,
         message: chatMessage.value,
       });
+      
+      //clear message field after submitting so user doesn't have to manually do it
       chatMessage.value='';
       chatMessage.focus();
-    }); //chatform event
+    }); 
 
     socket.on('updateMessages', function(data) {
       showMessage(data);
-    }); //updateMessages
-  } //chatform
-}); //socket
+    });
+  } 
+}); 
 
-function showMessage(data) {
+//DOM Management
+function showMessage(data) 
+{
   var chatDisplay = document.querySelector('.chat-display');
   var newMessage = document.createElement('p');
 
+  //give users different text colors
   if (chatUsername.value == data.username) {
     newMessage.className = 'bg-success chat-text';
-  } else {
+  } 
+  else {
     newMessage.className = 'bg-info text-warning chat-text';
   }
 
